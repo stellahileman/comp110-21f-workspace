b4c87f0530fa18378bfeb705541c6578ca817208
Stella Hileman <shileman@email.unc.edu>
2021-10-20 22:30:53 -0400

    finished ex06

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -1,5 +1,41 @@
 """Practice with dictionaries."""
 
-__author__ = "123456789"
+__author__ = "730400423"
 
-# Define your functions below
+
+def invert(dictionary: dict[str, str]) -> dict[str, str]:
+    """Invert the items in a dictionary."""
+    output = dict()
+    for key in dictionary:
+        if dictionary[key] in output:
+            raise KeyError("duplicates found")
+        output[dictionary[key]] = key
+    return output
+
+
+def favorite_color(dictionary: dict[str, str]) -> str:
+    """Finds color that is most frequent in a dictionary."""
+    color_counts: dict[str, int] = {}
+    for key in dictionary:
+        if dictionary[key] in color_counts:
+            color_counts[dictionary[key]] = color_counts[dictionary[key]] + 1
+        else:
+            color_counts[dictionary[key]] = 1
+    max_count: int = 0
+    fav_color: str = ""
+    for color in color_counts:
+        if color_counts[color] > max_count:
+            max_count = color_counts[color]
+            fav_color = color
+    return fav_color
+        
+
+def count(given: list[str]) -> dict[str, int]:
+    """Produces a dictionary from a list that counts how many times a value appears."""
+    counts: dict[str, int] = {}
+    for value in given:
+        if value in counts:
+            counts[value] += 1
+        else:
+            counts[value] = 1
+    return counts
--- exercises/ex06/dictionaries_test.py
+++ exercises/ex06/dictionaries_test.py
@@ -1,6 +1,54 @@
 """Unit tests for dictionary functions."""
 
-# TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex06.dictionaries import invert, favorite_color, count
+from exercises.ex06.dictionaries import invert, favorite_color, count
 
-__author__ = "123456789"
+__author__ = "730400423"
+
+
+def test_empty_dict_colors_invert() -> None:
+    """Testing that the first two functions return blank lines when given blank dictionaries."""
+    test: dict[str, str] = {}
+    assert invert(test) == {}
+    assert favorite_color(test) == ""
+
+
+def test_empty_list_count() -> None:
+    """Testing that the last function returns a blank dictionary when given a blank list."""
+    test: list[str] = []
+    assert count(test) == {}
+
+
+def test_long_dict_invert() -> None:
+    """Testing that invert works when given a long dictionary."""
+    test: dict[str, str] = {"Stella": "Green", "River": "Blue", "Claudia": "Yellow", "Everett": "Purple", "Phil": "Pink", "Max": "Red"}
+    assert invert(test) == {"Green": "Stella", "Blue": "River", "Yellow": "Claudia", "Purple": "Everett", "Pink": "Phil", "Red": "Max"}
+
+
+def test_long_dict_colors() -> None:
+    """Testing that color works when given a long dictionary."""
+    test: dict[str, str] = {"Stella": "Green", "River": "Blue", "Claudia": "Yellow", "Everett": "Green", "Phil": "Blue", "Max": "Green"}
+    assert favorite_color(test) == "Green"
+
+
+def test_long_list_count() -> None:
+    """Testing that count works when given a long list."""
+    test: list[str] = ["Green", "Blue", "Pink", "Blue", "Green", "Green"]
+    assert count(test) == {"Green": 3, "Blue": 2, "Pink": 1}
+
+
+def test_short_dict_invert() -> None:
+    """Testing that invert still works when given a short list."""
+    test: dict[str, str] = {"Stella": "Green"}
+    assert invert(test) == {"Green": "Stella"}
+
+
+def test_short_dict_colors() -> None:
+    """Testing that color still works when given a short list."""
+    test: dict[str, str] = {"Stella": "Green"}
+    assert favorite_color(test) == "Green"
+
+
+def test_short_list_count() -> None:
+    """Testing that count still works when given a short list."""
+    test: list[str] = ["Green"]
+    assert count(test) == {"Green": 1}
